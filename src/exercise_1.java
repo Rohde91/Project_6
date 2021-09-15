4db3b7a3d4772400bfa3ad7289d0dd9f9f15c884
Zaah91 <zaah91@gmail.com>
2021-09-15 10:47:24 +0200

    Exercise 1 done

--- src/exercise_1.java
+++ src/exercise_1.java
@@ -1,6 +1,6 @@
 public class exercise_1 {
     public static void main(String[] args) {
-        loop(10);
+        loop(57);
     }
 
     public static void loop(int n) {
@@ -13,5 +13,13 @@ public class exercise_1 {
                 i = i + 1;
             }
         }
+        // Output: 10, 5, 6, 3, 4, 2
+        // The while loop will run till i==0 by dividing whit 2 repeading until the value is 0
+        // If the loop cant divide by 2 whitout getting a odd number, it will add 1 to i
+        // Fx. it cant divide 5 by 2 whitout getting odd, so it then adds 1 to be able to divide
+        // test made whit n=100 and n=57, both whitout errors.
+
+        // n can be any positive number due to this, even or odd
+        // if it starts whit an odd number, it will just start at the else
     }
 }
