1992c2e3e74f2f035201ce75ec4efff39d771b9f
Martin Rohde <mart3b24@edu.easj.dk>
2021-09-23 10:28:43 +0200

    exersice 5 done

--- src/com/company/exercise_5.java
+++ src/com/company/exercise_5.java
@@ -2,19 +2,38 @@ package com.company;
 import java.util.Scanner;
 
 public class exercise_5 {
-    public static void isDoubloon(){
+    public static boolean isDoubloon(String s){
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++){
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++){
+                if (s.charAt(i) == s.charAt(j)){
+                    count ++;
+                }
+            }
+            if (count != 2){
+                return false;
+            }
+        }
+        return true;
 
     }
 
     public static void main(String[] args) {
 
-        Scanner.scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter characters.");
-        String input = scanner.nextLine();
+        String s = input.nextLine();
 
-        //toLowerCase skal bruges her for at omskrive input til lower case.
-        System.out.println(input.toLowerCase());
-        //Måske virker dette
+        if (isDoubloon(s)){
+            System.out.println("The word " + s + " is a doubloon.");
+        }
+        else {
+            System.out.println("The word " + s + " is not a doubloon.");
+        }
 
 
     }
---
+++ src/com/company/exersice_6.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class exersice_6 {
+
+    public static boolean canSpell(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
