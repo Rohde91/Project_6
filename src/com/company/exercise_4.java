f44508639720f5023a6aa07619c7605e929b5e77
Martin Rohde <mart3b24@edu.easj.dk>
2021-09-23 12:38:31 +0200

    exersice 6 done

--- src/com/company/exercise_4.java
+++ src/com/company/exercise_4.java
@@ -2,27 +2,30 @@ package com.company;
 import java.util.Scanner;
 
 public class exercise_4 {
-    /*public static void isAbecedarian(){
 
-    }*/
+    // Write a method called isAbecedarian that
+    // takes a String and
+    // returns a boolean indicating whether the word is abecedarian.
+
+    public static boolean isAbecedarian(String word){
+        for (int i = 1; i < word.length(); i++){
+            if (word.charAt(i) < word.charAt(i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the character :");
-        String input = scan.nextLine();
-        scan.close();
+        String word = "abc";
 
-        int index = input.length() - 1;
-        for (int i = 0; i < index; i++) {
-            if (input.charAt(i) <= input.charAt(i + 1)) {
-                //checking  the word, whether each letter is less than the before letter.
-
-            } else {
-                System.out.println("The  word is not abecedarian");
-                return;
-            }
+        if (isAbecedarian(word)){
+            System.out.println("The word " + word + " is Abecedarian");
         }
-        System.out.println("The  word is abecedarian");
+        else {
+            System.out.println("The word " + word + " is not Abecedarian");
+        }
+
     }
 }
--- src/com/company/exersice_6.java
+++ src/com/company/exersice_6.java
@@ -2,11 +2,42 @@ package com.company;
 
 public class exersice_6 {
 
-    public static boolean canSpell(){
+    public static boolean canSpell(String letters, String word){
+        boolean spell = true;
 
+        for (int i = 0; i <word.length(); i++){
+            char letter = word.charAt(i);
+            if (!letters.contains(String.valueOf(letter))){
+                spell = false;
+                break;
+            }
+            else {
+                letters = letters.replaceFirst(String.valueOf(word.charAt(i)), "");
+            }
+        }
+        return spell;
+    }
+
+    public static boolean canSpell2(String letterStack, String wordBuilding){
+        int numInWord = wordBuilding.length();
+        for (int i = 0; i <wordBuilding.length(); i++){
+            char currentLetters = wordBuilding.charAt(i);
+            for (int j = 0; j < letterStack.length(); j++){
+                if (currentLetters == letterStack.charAt(j)){
+                    //Removing from stack
+                    System.out.println(letterStack + " before removing.");
+                    letterStack = letterStack.substring(0,j) + "" + letterStack.substring(j+1);
+                    System.out.println(letterStack + " after removing");
+                    numInWord = numInWord - 1;
+                    System.out.println("Letters left: " + numInWord);
+                }
+            }
+        }
+        return numInWord == 0;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(canSpell2("hapsadoost", "ostehaaaps"));
+        // If both have equal values/letters. The result will come out as true.
     }
 }
